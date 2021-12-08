0b360bb0261a0b9b3bf240bc38905f7e7288dd66
paddich <skachkovaa.work@gmail.com>
2021-12-08 23:11:35 +0500

    +train_home.html +train button on layout

--- train_desk/train/views.py
+++ train_desk/train/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def train_home(request):
-    return HttpResponse('Крутые тренировки')
+    return render(request, 'train/train_home.html')
