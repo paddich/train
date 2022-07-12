a475d6b8008824e5446f53b3a5fbbc48164fb975
paddich <skachkovaa.work@gmail.com>
2022-07-12 14:32:26 +0500

    +train_home.html +train button on layout

--- train_desk/train/models.py
+++ train_desk/train/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# class Training(models.Model):
+#     title = # название тренировки, проверка на существующее, не обязательное поле
+#     # muscle = # определяется автоматически на основе выбранных упражнений и пока не нужно
+#     exercise = # добавить существующее упражнение из списка
+#     date = models.DateField(auto_now=True)
+#
+# class ExerciseEdit(models.Model):
+#     title = models.CharField(max_length=50) # название упражнения, проверка на существующее, обязательное поле
+#     # muscle = # пока не нужно, не обязательное поле
+#
+# class ExerciseWrite(models.Model):
+    # title =  уже созданное упражнение из списка
+    # weight =
+    # rep =
+    # approach = устанавливается по количеству пар wight + rep
+    # date = models.DateField(auto_now=True)
