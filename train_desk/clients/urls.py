ac42c46af7401c22a9e6e6986755ba68b3463136
paddich <skachkovaa.work@gmail.com>
2022-07-19 18:49:00 +0500

    +train_home.html +train button on layout

--- train_desk/clients/urls.py
+++ train_desk/clients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.index),
-    path('settings', views.settings)
-]
+urlpatterns = {
+    path('', views.index, name='clients_home'),
+    path('settings', views.settings, name='clients_set'),
+    path('client', views.client, name='client_add'),
+}
--- train_desk/clients/views.py
+++ train_desk/clients/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
     data = {
-        'title': 'Главная страница',
-        'values': ['Some', 'Hello', 'etc']
+        'title': 'Клиенты',
+        'clients_name': ['Петров', 'Сидоров', 'Козлов']
     }
     return render(request, 'clients/clients_home.html', data)
 
 def settings(request):
     return render(request, 'clients/settings.html')
+
+def client(request):
+    return render(request, 'clients/client_add.html')
