f037c4ffcc3da82834d975a3f75071fbf02d73c0
paddich <skachkovaa.work@gmail.com>
2022-07-23 15:28:34 +0500

    +train_home.html +train button on layout

--- train_desk/clients/models.py
+++ train_desk/clients/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField('имя', max_length=50)
+    name2 = models.CharField('фамилия', max_length=50)
+    name3 = models.CharField('отчество', max_length=50)
+    fullname = [name, name2, name3]
+    age = models.CharField('возраст', max_length=3)
+    date = models.DateTimeField('дата создания карточки')
+
+    # def __str__(self):
+    #     return self.fullname
+    #
+    # def get_absolute_url(self):
+    #     return f'/news/{self.id}'
+    #
+    # class Meta:
+    #     verbose_name = 'Клиенты'
+    #     verbose_name_plural = 'Клиенты'
+
+class Training(models.Model):
+    client_action = models.ForeignKey(Client, on_delete=models.CASCADE())
